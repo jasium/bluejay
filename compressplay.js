a3c6cd8f87d0987ef68f7657a4d834751581b50f
John M <jasium@gmail.com>
2014-02-09 07:46:15 -0500

    still not handling output right...

--- compressplay.js
+++ compressplay.js
@@ -2,26 +2,32 @@
  * Created by john on 2/6/14.
  */
 
+
 function updateURN() {
     var obj = {};
-    obj.level = document.getElementById("level");
-    obj.reverb = document.getElementById("reverb");
-    obj.dsp = document.getElementById("dsp");
+    obj.level = document.getElementById("level").value;
+    obj.reverb = document.getElementById("reverb").value;
+    obj.dsp = document.getElementById("dsp").value;
 
-    var json = JSON.stringify(obj)
-    alert(json);
+    var json;
+    json = JSON.stringify(obj);
+
+    //alert(json);
     var c = Iuppiter.compress(json);
-    window.confirm(c.length < s.length, c);
+    //window.confirm(c);
 
-    document.getElementById("urn").value = c;
-    var d = Iuppiter.decompress(c);
-    window.confirm(d == s, d);
+    //var d = Iuppiter.decompress(c);
+    //window.confirm(d);
 
     // Compressed byte array can be converted into base64 to sumbit to server side to do something.
     var b = Iuppiter.Base64.encode(c, true);
-
+    document.getElementById("urn").value = "urn:AUDLZ2:" + b;
+//
+//    window.confirm(b);
     var bb = Iuppiter.toByteArray(b);
     var db = Iuppiter.decompress(Iuppiter.Base64.decode(bb, true));
-    window.confirm(db == s, db);
+
+    document.getElementById("blob").value = db;
 
 }
+
